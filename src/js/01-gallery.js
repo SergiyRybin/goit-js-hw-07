41ff2ea06c47da05cfee0fd853397fb79a7a62c6
SergiyRybin <serhiobax@gmail.com>
2022-02-05 15:22:28 +0200

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,20 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery")
+
+const imageList = galleryItems.map( element=>{
+
+const link = document.createElement("a")
+link.classList.add("gallery__link")
+
+const img = document.createElement("img")
+img.classList.add("gallery__image")
+img.src = element.preview
+img.alt = element.description
+
+link.append(img)
+return link
+
+})
+gallery.append(...imageList)
