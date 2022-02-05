f21589898a273dcbdc05513fafc202f8fba2a49f
SergiyRybin <serhiobax@gmail.com>
2022-02-05 16:56:30 +0200

    й

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -8,10 +8,12 @@ const imageList = galleryItems.map( element=>{
 const link = document.createElement("a")
 link.classList.add("gallery__link")
 
+
 const img = document.createElement("img")
 img.classList.add("gallery__image")
 img.src = element.preview
 img.alt = element.description
+img.dataset.source = "large-image.jpg"
 
 link.append(img)
 return link
@@ -20,9 +22,9 @@ return link
 gallery.append(...imageList)
 
 
-document.querySelector('gallery__link').onclick = () => {
+// document.querySelector('gallery__link').onclick = () => {
 
-	basicLightbox.create(`
-		<img width="1400" height="900" src="galleryItems.original">
-	`).show()
-}
+// 	basicLightbox.create(`
+// 		<img width="1400" height="900" src="galleryItems.original">
+// 	`).show()
+// }
