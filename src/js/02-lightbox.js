09a9a0b43e8c2a38bf61af040112da04992392ea
SergiyRybin <serhiobax@gmail.com>
2022-02-05 16:15:37 +0200

    1

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -2,3 +2,29 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery")
+
+const imageList = galleryItems.map( element=>{
+
+const link = document.createElement("a")
+link.classList.add("gallery__link")
+
+const img = document.createElement("img")
+img.classList.add("gallery__image")
+img.src = element.preview
+img.alt = element.description
+
+link.append(img)
+return link
+
+})
+gallery.append(...imageList)
+
+
+document.querySelector('gallery__link').onclick = () => {
+
+	basicLightbox.create(`
+		<img width="1400" height="900" src="galleryItems.original">
+	`).show()
+}
