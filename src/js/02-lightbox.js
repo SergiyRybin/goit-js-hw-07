ff4c73f0ab906015c835f7fc22cc37b042c815e2
SergiyRybin <serhiobax@gmail.com>
2022-02-07 18:21:22 +0200

    Update 02-lightbox.js

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -6,25 +6,33 @@ const gallery = document.querySelector(".gallery")
 
 const imageList = galleryItems.map( element=>{
 
-const link = document.createElement("a")
+const list = document.createElement("li")
+
+  const link = document.createElement("a")
+
 link.classList.add("gallery__item")
 link.href = element.original
 
 const img = document.createElement("img")
+img.style.display = "block"
 img.classList.add("gallery__image")
 img.src = element.preview
 img.alt = element.description
-
+list.append(link)
 link.append(img)
-return link
+return list
 })
 
 gallery.append(...imageList)
 
 
 
-let ga = new SimpleLightbox('.gallery a', {
+let galleryModal = new SimpleLightbox('.gallery a', {
   caption: true,
   captionsData: 'alt',
   captionDelay: 250,
+  close: true,
+  closeText: "&#x2612;",
+  nav: true,
+  navText: ['&#8656;', ['&#8658;']]
 })
