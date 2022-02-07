4523d2a399806ecc771ceab86ada6cf37bbaabf3
SergiyRybin <serhiobax@gmail.com>
2022-02-07 18:05:48 +0200

    Update 02-lightbox.js

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,44 +1,30 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+
 const gallery = document.querySelector(".gallery")
 
 const imageList = galleryItems.map( element=>{
 
 const link = document.createElement("a")
-link.classList.add("gallery__link")
+link.classList.add("gallery__item")
+link.href = element.original
 
 const img = document.createElement("img")
 img.classList.add("gallery__image")
 img.src = element.preview
 img.alt = element.description
-img.dataset.source = element.original
 
 link.append(img)
 return link
-
 })
+
 gallery.append(...imageList)
 
 
 
-const galleryImage = document.querySelectorAll('.gallery__link img')
-let inst;
-const openModal = galleryImage.forEach((event)=>{
-event.addEventListener("click",()=> {
-inst= basicLightbox.create(`
-<div class="modal">
-    <img src=${event.dataset.source}>
-</div>
-`).show()
-const modal = document.querySelector(".modal")
-
-  })
+let ga = new SimpleLightbox('.gallery a', {
+  caption: true,
+  captionsData: 'alt',
+  captionDelay: 250,
 })
-
-
-// const closeModal = document.addEventListener("keydown", event =>{
-// 	if(event.code === "Escape")
-// 	modal.remove()
-//   })
-  
